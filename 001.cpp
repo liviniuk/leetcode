8b22b66c535ac85d599ad7e9a7e9b5b660244c65
Viktor Liviniuk <viktor.liviniuk@gmail.com>
2019-11-30 23:01:58 -0800

    Problem 1 - now using a map

--- 001.cpp
+++ 001.cpp
@@ -5,14 +5,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int size = nums.size();
-        for (int i = 0; i != size - 1; i++) {
-            for (int j = i + 1; j != size; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return vector<int>({i, j});
-                }
-            }
+        std::map<int, int> m;
+        int n = nums.size();
+        for (auto i = 0; i < n; ++i) {
+            int diff = target - nums[i];
+            if (m.find(diff) != m.end())
+                return std::vector<int>({m[diff], i});
+            m.insert(pair<int, int>(nums[i],i));
         }
+        return std::vector<int>({0,0});
     }
 };
 
